6ccf5d99003075b37ea909915e310fbc5adec67c
183082T <44395013+183082T@users.noreply.github.com>
2020-02-11 02:15:07 +0800

    sd dfa,

--- Tourlah/Tourlah/FavouriteEvents.aspx.designer.cs
+++ Tourlah/Tourlah/FavouriteEvents.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace WebApplication2 {
         protected global::System.Web.UI.WebControls.Label ErrorLabel;
         
         /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
         /// FavouriteGV control.
         /// </summary>
         /// <remarks>
